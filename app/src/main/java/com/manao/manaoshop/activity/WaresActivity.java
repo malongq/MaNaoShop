15c4b29ff93d7dd26d07f2ec9d57715e07bcff90
malong <malong@100tal.com>
2018-12-06 15:25:19 +0800

    继续

--- app/src/main/java/com/manao/manaoshop/activity/WaresActivity.java
+++ app/src/main/java/com/manao/manaoshop/activity/WaresActivity.java
@@ -96,19 +96,18 @@ public class WaresActivity extends AppCompatActivity {
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
-
         // title标题，微信、QQ和QQ空间等平台使用
         oks.setTitle(getString(R.string.share));
         // titleUrl QQ和QQ空间跳转链接
-        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl("https://github.com/malongq");
         // text是分享文本，所有平台都需要这个字段
         oks.setText("我是分享文本");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        //确保存在此张图片
+        oks.setImageUrl(wares.getImgUrl());
         // url在微信、微博，Facebook等平台中使用
-        oks.setUrl("http://sharesdk.cn");
+        oks.setUrl("https://github.com/malongq");
         // comment是我对这条分享的评论，仅在人人网使用
-        oks.setComment("我是测试评论文本");
+        oks.setComment("我是玛瑙我是玛瑙我是玛瑙我是玛瑙");
         // 启动分享GUI
         oks.show(this);
     }
